cde5120b0fca9d4ad1c0358c085db3696fc149f5
xMegumi <lorenzo.vdkn@gmail.com>
2025-12-01 09:50:01 +0100

    feat: Refactor event management methods to use EventService and remove unused code

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,12 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Event;
-use App\Entity\Inscription;
 use App\Form\EventType;
 use App\Repository\EventRepository;
 use App\Repository\InscriptionRepository;
 use App\Service\EventService;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -52,7 +50,7 @@ class EventController extends AbstractController
     }
 
     #[Route('/new', name: 'app_event_new')]
-    public function new(Request $request, EntityManagerInterface $em): Response
+    public function new(Request $request, EventService $eventService): Response
     {
         $event = new Event();
         $form = $this->createForm(EventType::class, $event);
@@ -60,22 +58,9 @@ class EventController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $file = $form->get('imageFile')->getData();
-
-            if ($file) {
-                $filename = uniqid() . '.' . $file->guessExtension();
-
-                $file->move(
-                    $this->getParameter('uploads_directory'),
-                    $filename
-                );
-
-                $event->setImage($filename);
-            }
             
             $event->setCreatedBy($this->getUser());
-            $event->setCreatedAt(new \DateTimeImmutable());
-            $em->persist($event);
-            $em->flush();
+            $eventService->createEvent($event, $file);
 
             return $this->redirectToRoute('app_my_events');
         }
@@ -109,7 +94,7 @@ class EventController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_event_edit', requirements: ['id' => '\d+'])]
-    public function edit(Request $request, Event $event, EntityManagerInterface $em): Response
+    public function edit(Request $request, Event $event, EventService $eventService): Response
     {
         // Vérifier que l'utilisateur est le créateur de l'événement
         if ($event->getCreatedBy() !== $this->getUser()) {
@@ -122,27 +107,8 @@ class EventController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $file = $form->get('imageFile')->getData();
-
-            if ($file) {
-                // Supprimer l'ancienne image si elle existe
-                if ($event->getImage()) {
-                    $oldFile = $this->getParameter('uploads_directory') . '/' . $event->getImage();
-                    if (file_exists($oldFile)) {
-                        unlink($oldFile);
-                    }
-                }
-
-                $filename = uniqid() . '.' . $file->guessExtension();
-
-                $file->move(
-                    $this->getParameter('uploads_directory'),
-                    $filename
-                );
-
-                $event->setImage($filename);
-            }
             
-            $em->flush();
+            $eventService->updateEvent($event, $file);
 
             $this->addFlash('success', 'L\'événement a été modifié avec succès.');
             return $this->redirectToRoute('app_my_events');
@@ -155,7 +121,7 @@ class EventController extends AbstractController
     }
 
     #[Route('/{id}/delete', name: 'app_event_delete', methods: ['POST'], requirements: ['id' => '\d+'])]
-    public function delete(Request $request, Event $event, EntityManagerInterface $em): Response
+    public function delete(Request $request, Event $event, EventService $eventService): Response
     {
         // Vérifier que l'utilisateur est le créateur de l'événement
         if ($event->getCreatedBy() !== $this->getUser()) {
@@ -165,17 +131,7 @@ class EventController extends AbstractController
 
         // Vérifier le token CSRF
         if ($this->isCsrfTokenValid('delete'.$event->getId(), $request->request->get('_token'))) {
-            // Supprimer l'image si elle existe
-            if ($event->getImage()) {
-                $imageFile = $this->getParameter('uploads_directory') . '/' . $event->getImage();
-                if (file_exists($imageFile)) {
-                    unlink($imageFile);
-                }
-            }
-
-            $em->remove($event);
-            $em->flush();
-
+            $eventService->deleteEvent($event);
             $this->addFlash('success', 'L\'événement a été supprimé avec succès.');
         }
 
@@ -183,7 +139,7 @@ class EventController extends AbstractController
     }
 
     #[Route('/{id}/inscrire', name: 'app_event_inscrire', methods: ['POST'], requirements: ['id' => '\d+'])]
-    public function inscrire(Request $request, Event $event, EntityManagerInterface $em, InscriptionRepository $inscriptionRepository): Response
+    public function inscrire(Request $request, Event $event, EventService $eventService): Response
     {
         // Vérifier que l'utilisateur est connecté
         $user = $this->getUser();
@@ -198,32 +154,18 @@ class EventController extends AbstractController
             return $this->redirectToRoute('app_event_show', ['id' => $event->getId()]);
         }
 
-        // Vérifier si l'utilisateur est déjà inscrit
-        $existingInscription = $inscriptionRepository->findOneBy([
-            'user' => $user,
-            'event' => $event
-        ]);
-
-        if ($existingInscription) {
-            $this->addFlash('warning', 'Vous êtes déjà inscrit à cet événement.');
-            return $this->redirectToRoute('app_event_show', ['id' => $event->getId()]);
+        try {
+            $eventService->inscribeUser($event, $user);
+            $this->addFlash('success', 'Vous êtes maintenant inscrit à cet événement !');
+        } catch (\Exception $e) {
+            $this->addFlash('warning', $e->getMessage());
         }
 
-        // Créer l'inscription
-        $inscription = new Inscription();
-        $inscription->setUser($user);
-        $inscription->setEvent($event);
-        $inscription->setInscritAt(new \DateTimeImmutable());
-
-        $em->persist($inscription);
-        $em->flush();
-
-        $this->addFlash('success', 'Vous êtes maintenant inscrit à cet événement !');
         return $this->redirectToRoute('app_event_show', ['id' => $event->getId()]);
     }
 
     #[Route('/{id}/desinscrire', name: 'app_event_desinscrire', methods: ['POST'], requirements: ['id' => '\d+'])]
-    public function desinscrire(Request $request, Event $event, EntityManagerInterface $em, InscriptionRepository $inscriptionRepository): Response
+    public function desinscrire(Request $request, Event $event, EventService $eventService): Response
     {
         // Vérifier que l'utilisateur est connecté
         $user = $this->getUser();
@@ -238,22 +180,13 @@ class EventController extends AbstractController
             return $this->redirectToRoute('app_event_show', ['id' => $event->getId()]);
         }
 
-        // Trouver l'inscription
-        $inscription = $inscriptionRepository->findOneBy([
-            'user' => $user,
-            'event' => $event
-        ]);
-
-        if (!$inscription) {
-            $this->addFlash('warning', 'Vous n\'êtes pas inscrit à cet événement.');
-            return $this->redirectToRoute('app_event_show', ['id' => $event->getId()]);
+        try {
+            $eventService->unsubscribeUser($event, $user);
+            $this->addFlash('success', 'Vous êtes maintenant désinscrit de cet événement.');
+        } catch (\Exception $e) {
+            $this->addFlash('warning', $e->getMessage());
         }
 
-        // Supprimer l'inscription
-        $em->remove($inscription);
-        $em->flush();
-
-        $this->addFlash('success', 'Vous êtes maintenant désinscrit de cet événement.');
         return $this->redirectToRoute('app_event_show', ['id' => $event->getId()]);
     }
 }
